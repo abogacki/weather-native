c65333cca89d67885d2ed4df80ce85e010b16fcb
abogacki <andrzej.bogacki@intive.com>
2019-08-05 10:26:48 +0200

    Initial commit, adds ts config adds react-native-elements library

---
+++ src/App.tsx
@@ -0,0 +1,90 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * Generated with the TypeScript template
+ * https://github.com/emin93/react-native-template-typescript
+ *
+ * @format
+ */
+
+import React, {Fragment} from 'react'; 
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  View,
+} from 'react-native';
+
+import {Button, ThemeProvider} from 'react-native-elements'
+
+import {
+  Colors,
+  Header,
+} from 'react-native/Libraries/NewAppScreen';
+import RaisedButton from './components/RaisedButton';
+
+
+const App = () => {
+  return (
+    <Fragment>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        <ScrollView
+          contentInsetAdjustmentBehavior="automatic"
+          style={styles.scrollView}>
+          <Header />
+          <View style={styles.body}>
+            <View style={styles.sectionContainer}>
+            <ThemeProvider>
+              <Button title="Hey" />
+            </ThemeProvider>
+            </View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    </Fragment>
+  );
+};
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: Colors.lighter,
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: Colors.white,
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+    color: Colors.black,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: Colors.dark,
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: Colors.dark,
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+});
+
+export default App;
---
+++ src/components/RaisedButton.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+import { Button } from 'react-native-elements';
+
+// tslint:disable-next-line: jsx-boolean-value
+const RaisedButton = (props: any) => <Button raised {...props} />
+
+export default RaisedButton
